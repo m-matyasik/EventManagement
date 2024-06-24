451051c1cbb36f5bd11588da2fc59302e5b1e47e
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2024-06-25 00:34:56 +0200

    Implement importants for Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,62 @@
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Application;
+using Infrastructure.Data;
+using Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Event Mgmt API", Version = "v1"});
+    
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer abcdefgh12345\"",
+    });
+    
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        Array.Empty<string>()
+        }
+    });
+    
+   var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+   var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+   c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services
+    .AddJwtAuthentication(builder.Configuration)
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
@@ -22,4 +73,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
 app.Run();
