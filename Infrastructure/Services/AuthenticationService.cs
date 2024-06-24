215ff3a250185bc6ae8174ea89c173cf014e739b
mariuszmatyasik <mariusz.matyasik@microsoft.wsei.edu.pl>
2024-06-25 00:33:18 +0200

    Auth service impl

--- Infrastructure/Services/AuthenticationService.cs
+++ Infrastructure/Services/AuthenticationService.cs
@@ -1,6 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities;
+using Infrastructure.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
 namespace Infrastructure.Services;
 
-public class AuthenticationService
+public class AuthenticationService(JwtSettings jwtSettings)
 {
-    
+    public string GenerateJwtToken(User user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Role, user.Role.ToString()),
+        };
+
+        var token = new JwtSecurityToken(
+            jwtSettings.Issuer,
+            jwtSettings.Audience,
+            claims,
+            expires: DateTime.UtcNow.AddMinutes(jwtSettings.ExpiryMinutes),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token)!;
+    }
 }
